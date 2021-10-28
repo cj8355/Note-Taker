a64aa33cecc26fa68e3f9d31de9f890a5b89439f
Charles Callender <charlescallender.cc@gmail.com>
2021-10-28 16:22:48 -0700

    Got the note file to deploy to localhost. Still need to get it deployed to Heroku and generate unique ids for the saved notes.

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+//Requiring dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//Require routes file
+require('./routes/api/routes')(app);
+
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
